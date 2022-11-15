36829d1a793cfd28272f1da99cf702e313347174
Court Jester <86880403+vim-clown@users.noreply.github.com>
2022-11-15 21:06:47 +0300

    -

--- O_0/__init__.py
+++
@@ -1,2 +0,0 @@
-from O_0.auth import api
-from O_0.destroy import softblock
--- O_0/destroy.py
+++
@@ -1,5 +0,0 @@
-import O_0
-
-def softblock(user_id, screen_name):
-    O_0.api.CreateBlock(user_id=user_id, screen_name=screen_name)
-    O_0.api.DestroyBlock(user_id=user_id, screen_name=screen_name)
